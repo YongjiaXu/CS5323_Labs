5cf70f1e76c8e5e5a829e5fd673ab95a07b45401
John Zhang <johnzhang@JohndeMacBook-Pro.local>
2021-09-26 14:45:07 -0500

    Add ToneAnalysis

---
+++ AudioLabSwift/AudioLabSwift/ToneAnalysis.swift
@@ -0,0 +1,27 @@
+//
+//  ToneAnalysis.swift
+//  AudioLabSwift
+//
+//  Created by John Zhang on 9/25/21.
+//  Copyright © 2021 John Zhang. All rights reserved.
+//
+
+import Foundation
+import Accelerate
+
+class ToneAnalysis {
+    
+    var fftData:[Float]
+    
+    init (audioFftData: Array<Float>){
+        fftData = audioFftData
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
