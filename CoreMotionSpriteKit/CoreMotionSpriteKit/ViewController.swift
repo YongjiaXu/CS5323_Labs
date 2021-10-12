31c1b7b9d6db7de3e688c065ba4d124d3c301ee7
John Zhang <johnzhang@JohndeMacBook-Pro.local>
2021-10-11 20:09:25 -0500

    Add mod A

--- CoreMotionSpriteKit/CoreMotionSpriteKit/ViewController.swift
+++ CoreMotionSpriteKit/CoreMotionSpriteKit/ViewController.swift
@@ -11,13 +11,18 @@ import CoreMotion
 class ViewController: UIViewController {
 
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var currentStepLabel: UILabel!
+    @IBOutlet weak var yesterdayStepLabel: UILabel!
     
     let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startActivityMonitoring()
+        startPedometerMonitoring()
     }
 
 
@@ -51,6 +56,39 @@ class ViewController: UIViewController {
             }
             
         }
+    
+    func startPedometerMonitoring(){
+            // check if pedometer is okay to use
+            if CMPedometer.isStepCountingAvailable(){
+                guard let pastDate = Calendar.current.date(byAdding: .day, value: -1, to: Date()) else{
+                    return
+                }
+                                                      
+                pedometer.queryPedometerData(from: Calendar.current.startOfDay(for: pastDate), to: pastDate)
+                {(pedData:CMPedometerData?, error:Error?)->Void in
+                    if let data = pedData {
+                        
+                        // display the output directly on the phone
+                          DispatchQueue.main.async {
+                            self.yesterdayStepLabel.text = "\(data.numberOfSteps.intValue)"
+                        }
+                    }
+                }
+                pedometer.queryPedometerData(from: Calendar.current.startOfDay(for: Date()), to: Date())
+                {(pedData:CMPedometerData?, error:Error?)->Void in
+                    if let data = pedData {
+                        
+                        // display the output directly on the phone
+                          DispatchQueue.main.async {
+                            self.currentStepLabel.text = "\(data.numberOfSteps.intValue)"
+                        }
+                    }
+                }
+                
+            }
+        }
+
+    
     
 }
 
