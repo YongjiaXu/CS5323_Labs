34e16326d138a8f1d9d00e15f582c250eeacda1d
John Zhang <johnzhang@JohndeMacBook-Pro.local>
2021-10-27 15:42:51 -0500

    add modA

--- ImageLab/ImageLab/ViewController.swift
+++ ImageLab/ImageLab/ViewController.swift
@@ -15,10 +15,23 @@ class ViewController: UIViewController   {
     var filters : [CIFilter]! = nil
     var videoManager:VideoAnalgesic! = nil
     let pinchFilterIndex = 2
-    var detector:CIDetector! = nil
     var isovering = false
     var notoveringtimer = 0
     let bridge = OpenCVBridge()
+    
+    lazy var detector:CIDetector! = {
+            // create dictionary for face detection
+            // HINT: you need to manipulate these properties for better face detection efficiency
+            let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyHigh, CIDetectorTracking:true] as [String : Any]
+            
+            // setup a face detector in swift
+            let detector = CIDetector(ofType: CIDetectorTypeFace,
+                                      context: self.videoManager.getCIContext(), // perform on the GPU is possible
+                options: (optsDetector as [String : AnyObject]))
+            
+            return detector
+        }()
+    
     @IBOutlet weak var togglecamera: UIButton!
     
     @IBOutlet weak var toggleflash: UIButton!
@@ -31,6 +44,8 @@ class ViewController: UIViewController   {
         super.viewDidLoad()
         
         self.view.backgroundColor = nil
+        self.setupFilters()
+
         
         // setup the OpenCV bridge nose detector, from file
         self.bridge.loadHaarCascade(withFilename: "nose")
@@ -38,13 +53,7 @@ class ViewController: UIViewController   {
         self.videoManager = VideoAnalgesic(mainView: self.view)    // Change the camera to back
         self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
         
-
-        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow,CIDetectorTracking:true] as [String : Any]
         
-        // setup a face detector in swift
-        self.detector = CIDetector(ofType: CIDetectorTypeFace,
-                                  context: self.videoManager.getCIContext(), // perform on the GPU is possible
-            options: (optsDetector as [String : AnyObject]))
         
         self.videoManager.setProcessingBlock(newProcessBlock: self.processImageSwift)
         
@@ -54,46 +63,16 @@ class ViewController: UIViewController   {
     
     }
     
-    //MARK: Process image output
-    func processImageSwift(inputImage:CIImage) -> CIImage{
-        
-
-        var retImage = inputImage
-        self.bridge.setTransforms(self.videoManager.transform)
-        self.bridge.setImage(retImage,
-                             withBounds: retImage.extent,
-                             andContext: self.videoManager.getCIContext())
-        
-        let gotfinger = self.bridge.processFinger()     // Let the function process finger
-        retImage = self.bridge.getImageComposite()
-        
-
-        DispatchQueue.main.async { // Make sure you're on the main thread here
-            self.togglecamera.isEnabled = !gotfinger  // enable/disable the camera based on the finger overing
-            self.toggleflash.isEnabled = !gotfinger  // enable/disable the flash based on the finger overing
-            if(gotfinger && !self.isovering){     // If it got the finger for the first time
-                _ = self.videoManager.turnOnFlashwithLevel(1.0) // Turn on the flash
-                self.isovering = true              // Mark it as overing
-            }
-            else if(self.isovering && !gotfinger){  // If it is overing not it cannot detect it
-                if(self.notoveringtimer > 5){      // If it fails the detection for 5 times
-                    self.videoManager.turnOffFlash()   // turn off the camera
-                    self.isovering = false              // mark it as not overing
-                    self.notoveringtimer = 0            // reset the timer
-                }
-                else{                    // wait to see if stil non-detected next time
-                    self.notoveringtimer = self.notoveringtimer + 1;
-                }
-            }
-            // Note: When you test the Part 4.1, it may seem like intermittent, the problem is when the flash is on the image will become more bright which reduce the intensity of red. Try use one finger or not let the finger overing the flash.
+    //MARK: Setup filtering
+        func setupFilters(){
+            filters = []
+            
+            let filterPinch = CIFilter(name:"CIBumpDistortion")!
+            filterPinch.setValue(-0.5, forKey: "inputScale")
+            filterPinch.setValue(75, forKey: "inputRadius")
+            filters.append(filterPinch)
+            
         }
-        
-        
-        
-        return retImage
-    }
-    
-    //MARK: Setup Face Detection
     
     func getFaces(img:CIImage) -> [CIFaceFeature]{
         // this ungodly mess makes sure the image is the correct orientation
@@ -102,6 +81,43 @@ class ViewController: UIViewController   {
         return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
         
     }
+    
+    func applyFiltersToFaces(inputImage:CIImage,features:[CIFaceFeature])->CIImage{
+            var retImage = inputImage
+            var filterCenter = CGPoint()
+            
+            for f in features {
+                //set where to apply filter
+                filterCenter.x = f.bounds.midX
+                filterCenter.y = f.bounds.midY
+                
+                //do for each filter (assumes all filters have property, "inputCenter")
+                for filt in filters{
+                    filt.setValue(retImage, forKey: kCIInputImageKey)
+                    filt.setValue(CIVector(cgPoint: filterCenter), forKey: "inputCenter")
+                    // could also manipulate the radius of the filter based on face size!
+                    retImage = filt.outputImage!
+                }
+            }
+            return retImage
+        }
+    
+    //MARK: Process image output
+    func processImageSwift(inputImage:CIImage) -> CIImage{
+        
+        // detect faces
+                let faces = getFaces(img: inputImage)
+                
+                // if no faces, just return original image
+                if faces.count == 0 { return inputImage }
+                
+                //otherwise apply the filters to the faces
+                return applyFiltersToFaces(inputImage: inputImage, features: faces)
+    }
+    
+    //MARK: Setup Face Detection
+    
+    
     
     
     // change the type of processing done in OpenCV
